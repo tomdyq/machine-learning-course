e423045d5f3049169b07bed716e151dd05aef4f8
LichenGituser <1301221238@pub.ss.pku.edu.cn>
2015-03-19 21:12:31 +0800

    add priori primer methods

--- lib/Apriori/apriori.py
+++ lib/Apriori/apriori.py
@@ -36,5 +36,32 @@ def scanD(D, Ck, minSupport):
 			retList.insert(0, key)
 		supportData[key] = support_cur
 	return retList, supportData 
+
+def aprioriGen(Lk, k):
+	retList = []
+	lenLk = len(Lk)
+
+	for i in range(lenLk):
+		for j in range(i+1, lenLk):
+			L1 = list(Lk[i+1])[:k-2]; L2 = list(Lk[j])[:k-2]
+			L1.sort(); L2.sort()
+			if L1 == L2:
+				retList.append(Lk[i] | Lk[j])
+	return retList
+
+def apriori(dataSet, minSupport = 0.5):
+	C1 = createC1(dataSet)
+	D = map(set, dataSet)
+	L1, supportData = scanD(D, C1, minSupport)
+	L = [L1]	
+	k = 2
+	while (len(L[k-2]) >0):
+		Ck = aprioriGen(L[k-2], k)
+		Lk, supK = scanD(D, Ck, minSupport)	
+		supportData.update(supK)
+		L.append(Lk)
+		k += 1
+	return L, supportData
 	
+		
 

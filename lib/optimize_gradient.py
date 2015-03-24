b4cb2663487dc5c4124c4898620d8a4cc712d717
LichenGituser <1301221238@pub.ss.pku.edu.cn>
2015-03-24 19:10:42 +0800

    Delete optimize_gradient.py

--- lib/optimize_gradient.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-
-import scipy.optimize, scipy.special
-from numpy import *
-
-def optimize_gradient(costFunction, X, y, theta, iterator, output_status):
-	result = scipy.optimize.fmin(costFunction, x0=theta, args = (X, y), maxiter=iterator, full_output=output_status)
-	return result[0], result[1]
-	

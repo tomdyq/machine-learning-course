081557ffd55f9e3aea68fd10fb3ea90f7ba06332
tomdyq <tomdyq@163.com>
2015-09-22 13:47:22 +0000

    add andrew ng ex1 ex2

--- examples/ID3/unit.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-from trees import *
-
-def main():
-	myDat, labels = createDataSet()
-	myTree = createTree(myDat, labels)
-	print myTree
-if __name__ == '__main__':
-	main()
--- examples/LogisticRegression/README.cmd
+++
@@ -1,2 +0,0 @@
-build a logistic regression model to predict whether a student gets admitted into a university
-$ ./logistic.py
